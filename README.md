b4f71fbd10329c17cc3b1273ce83a04ae48e5733
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-08-22 21:19:28 -0300

    Implements lonelyinteger

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lonely Integer
+Exercise from [HackerRank](https://www.hackerrank.com/challenges/three-month-preparation-kit-lonely-integer/problem)
---
+++ src/main/java/Solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+        Set<Integer> unique = new HashSet<>();
+        for (int n : a){
+            if (!unique.add(n)) unique.remove(n);
+        }
+        return unique.iterator().next();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
